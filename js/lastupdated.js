007a108f7c22e39357f8f121b7e14eaf9f69e039
ProjectByMeg <nun19002@byui.edu>
2020-01-28 17:48:29 -0700

    Commit yas

--- js/lastupdated.js
+++ js/lastupdated.js
@@ -1,2 +1,6 @@
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentyear").textContent = year;
+
 let lastUpdated = document.lastModified;
 document.getElementById("lastupdated").textContent = lastUpdated;
