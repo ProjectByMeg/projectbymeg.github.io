8304bacce412095ad4ec755b75973f8eb6edebc9
ProjectByMeg <nun19002@byui.edu>
2020-02-15 23:48:31 -0700

    push

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,12 @@
-function windchill() {
-    let temperature = parseInt(document.getElementById("high").innerText);
-    let windspeed = parseInt(document.getElementById("windspeed").innerText);
-    let result = windChill(temperature, windspeed)
-    document.getElementById("windchill").innerText = result; 
-}
+let tempF = parseInt(document.getElementById("high").innerText);
+let speed = parseInt(document.getElementById("windspeed").innerText);
 
-function windChill(tempF, speed) {
-   let wind = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
-   return wind;
+
+if (tempF <= 50 && speed >= 3) {
+    let windchill = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+
+    document.getElementById("windchill").innerHTML = windchill.toFixed(2);
+} 
+else {
+    document.getElementById("windchill").innerText = "N/A"
 }
