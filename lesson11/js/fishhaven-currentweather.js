493c40a14cf58abd96fe8f932ccee1ee760bf3bf
ProjectByMeg <nun19002@byui.edu>
2020-04-10 06:43:53 -0700

    push

---
+++ lesson11/js/fishhaven-currentweather.js
@@ -0,0 +1,12 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=d406709765ef36a2535d61a592b024b7"
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('condition').textContent = jsObject.weather[0].description;
+    document.getElementById('current').textContent = Math.round(jsObject.main.temp);
+    document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('windspeed').textContent = jsObject.wind.speed;
+  });
+
---
+++ lesson11/js/fishhaven-forecast.js
@@ -0,0 +1,16 @@
+const apiURL1 = 'https://api.openweathermap.org/data/2.5/forecast?zip=94040,us&units=imperial&APPID=d406709765ef36a2535d61a592b024b7';
+fetch(apiURL1)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        let fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+        for (let i = 0; i < fivedayforecast.length; i++) {
+          let d = new Date(fivedayforecast[i].dt_txt);
+          let dayName = days[d.getDay()];
+          document.getElementById("day" + i).textContent = dayName;
+          document.getElementById("forecast" + i).innerHTML = parseFloat(fivedayforecast[i].main.temp).toFixed(0) + "&#8457;";
+          document.getElementById("icon" + i).setAttribute("src", `https://openweathermap.org/img/w/${fivedayforecast[i].weather[0].icon}.png`);
+          document.getElementById("icon" + i).setAttribute("alt", fivedayforecast[i].weather[0].description);
+        }
+      });
