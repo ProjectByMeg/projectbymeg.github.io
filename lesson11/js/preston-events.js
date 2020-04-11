56599a7a06b40182fc401a8107d6144853e67a43
ProjectByMeg <nun19002@byui.edu>
2020-04-11 10:55:23 -0700

    weather site

---
+++ lesson11/js/preston-events.js
@@ -0,0 +1,31 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        //console.log(response);
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        //console.table(jsonObject);
+        const towns = jsonObject['towns'];
+        for (let i = 0; i < towns.length; i++) {
+            if (towns[i].name == "Preston") {
+                let article = document.createElement('section');
+                let header = document.createElement('h3');
+                let events0 = document.createElement('p');
+                let events1 = document.createElement('p');
+                let events2 = document.createElement('p');
+
+                header.textContent = 'Events';
+                events0.textContent = towns[i].events[0];
+                events1.textContent = towns[i].events[1];
+                events2.textContent = towns[i].events[2];
+
+                article.appendChild(header);
+                article.appendChild(events0);
+                article.appendChild(events1);
+                article.appendChild(events2);
+
+                document.querySelector('.events').appendChild(article);
+            }
+        }
+    });
