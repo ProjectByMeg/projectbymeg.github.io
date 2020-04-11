678cb74e62d38a0fcb9150d16906351737e60e77
ProjectByMeg <nun19002@byui.edu>
2020-04-11 02:51:19 -0700

    salmon-adventures

--- salmon-adventures/js/weather.js
+++ salmon-adventures/js/weather.js
@@ -6,18 +6,4 @@ fetch(apiURL)
         document.getElementById('condition').textContent = jsObject.weather[0].description;
         document.getElementById('current').textContent = Math.round(jsObject.main.temp);
         document.getElementById('high').textContent = Math.round(jsObject.main.temp_max);
-        document.getElementById('humidity').textContent = jsObject.main.humidity;
-        document.getElementById('windspeed').textContent = jsObject.wind.speed;
     });
-
-let tempF = parseInt(document.getElementById("high").innerText);
-let speed = parseInt(document.getElementById("windspeed").innerText);
-
-
-if (tempF <= 50 && speed >= 3) {
-    let windchill = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
-
-    document.getElementById("windchill").innerHTML = windchill.toFixed(2);
-} else {
-    document.getElementById("windchill").innerText = "N/A"
-}
