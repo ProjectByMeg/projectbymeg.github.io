e040688a482e4804b2f0415e407ddf2ac188d8c2
ProjectByMeg <nun19002@byui.edu>
2020-04-12 19:40:09 -0700

    weather site

--- lesson11/js/fishhaven-currentweather.js
+++ lesson11/js/fishhaven-currentweather.js
@@ -1,4 +1,4 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid=d406709765ef36a2535d61a592b024b7"
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?zip=94040,us&units=imperial&appid=d406709765ef36a2535d61a592b024b7"
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
